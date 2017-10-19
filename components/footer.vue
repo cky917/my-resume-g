b3f44adde8ec0d5a88cb6d53f10e26a54645b5c9
cky <576779975@qq.com>
2017-10-19 20:31:05 +0800

    modify:footer

--- components/footer.vue
+++ components/footer.vue
@@ -1,6 +1,6 @@
 <template>
   <footer>
-        © {{ baseInfo.name }} 的简历 Powered by <a href="https://github.com/cky917/me" target="_blank">me,简单的配置化网页简历生成系统</a>
+        © {{ baseInfo.name }} 的简历 Powered by <a href="https://github.com/cky917/my-resume-g" target="_blank">me,简单的配置化网页简历生成系统</a>
   </footer>
 </template>
 <script>
